22ad2c3f5bcebc985641e46773130e70c353ff98
JoacoMaurtua <joacomaurtua@gmail.com>
2022-08-31 10:41:45 -0500

    todos los archivos

--- App.js
+++ App.js
@@ -1,20 +1,60 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button, TextInput } from 'react-native'; //componentes nativos de react native
 
+// View hace el papel de div,section,container y de cualquier contenedor que agrupe mas elementos
+//flex ya esta por defecto, se pueden usar las propiedades flex-box de frente
+// Los botones no tienen un prop de estilos, se les da a traves de atributos
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.appContainer}>
+      <View style={styles.inputContainer}>
+        <TextInput 
+          style={styles.textInput}
+          placeholder="Your week goal"
+        />
+        <Button 
+          title="Add Goal"
+          padding="2"
+       
+        />
+      </View>
+      <View style={styles.goalsContainer}>
+        <Text>List of goals...</Text>  
+      </View>
+      
     </View>
   );
 }
 
+
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  appContainer: {
+    flex:1,
+    paddingTop:50,
+    paddingHorizontal:16
   },
+
+  inputContainer:{
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    marginBottom: 24,
+    borderBottomWidth: 1,
+    borderBottomColor: "#cccccc"
+  },
+
+  textInput:{
+    borderWidth: 1,
+    borderColor: '#cccccc',
+    width: '70%',
+    height: 40,
+    marginRight: 8,
+    paddingLeft: 10,
+    flex:1
+  },
+
+  goalsContainer:{
+    flex: 4,
+  }
+ 
 });
